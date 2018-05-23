1505e77f1c4a0e979b0746dece5a70bb4b8eb4f6
Andrew Barba <abarba.77@gmail.com>
2018-05-23 17:33:40 -0400

    implement new session settings

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # PT Defender Core
 
 Core component for PT Defender
+
+### Custom Environment
+
+```sh
+env $(cat .env | xargs) npm start
+```
--- bin/cli.js
+++ bin/cli.js
@@ -4,23 +4,18 @@ const args = require('yargs-parser')(process.argv.slice(2), {
     url: process.env.PT_URL || 'http://localhost:8081',
     serverToken: process.env.PT_SERVER_TOKEN,
     license: process.env.PT_DEFENDER_LICENSE,
+    session: process.env.PT_DEFENDER_SESSION,
     exchange: process.env.PT_DEFENDER_EXCHANGE,
     tradingKey: process.env.PT_DEFENDER_EXCHANGE_KEY,
     tradingSecret: process.env.PT_DEFENDER_EXCHANGE_SECRET
   }
 })
 
+const packageJson = require('../package')
 const Core = require('../lib')
 const core = new Core(args)
 const { info } = core.logger
-
-info()
-info()
-info('/*---------------------------------*')
-info(' *           PT Defender           *')
-info(' *---------------------------------*/')
-info()
-info()
+const { log } = console
 
 core.api.me()
   .then(user => info('Logged in as:', user.email))
@@ -28,3 +23,18 @@ core.api.me()
   .then(data => info('Connected to ProfitTrailer:', data.selectedConfig))
   .then(() => core.start())
   .then(() => info('PT Defender running...'))
+
+log(`
+
+ /$$$$$$$  /$$$$$$$$       /$$$$$$$             /$$$$$$                          /$$
+| $$__  $$|__  $$__/      | $$__  $$           /$$__  $$                        | $$
+| $$  \\ $$   | $$         | $$  \\ $$  /$$$$$$ | $$  \\__//$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$
+| $$$$$$$/   | $$         | $$  | $$ /$$__  $$| $$$$   /$$__  $$| $$__  $$ /$$__  $$ /$$__  $$ /$$__  $$
+| $$____/    | $$         | $$  | $$| $$$$$$$$| $$_/  | $$$$$$$$| $$  \\ $$| $$  | $$| $$$$$$$$| $$  \\__/
+| $$         | $$         | $$  | $$| $$_____/| $$    | $$_____/| $$  | $$| $$  | $$| $$_____/| $$
+| $$         | $$         | $$$$$$$/|  $$$$$$$| $$    |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$| $$
+|__/         |__/         |_______/  \\_______/|__/     \\_______/|__/  |__/ \\_______/ \\_______/|__/
+
+v${packageJson.version}
+
+`)
